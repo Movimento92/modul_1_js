de7cad1095005815150ce1f19ced2567fb3d7abb
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-04-16 23:10:18 +0300

    модуль1

---
+++ modul_1.js
@@ -0,0 +1,174 @@
+// Задание 1
+
+const productName = 'Droid';
+const pricePerItem = 2000;
+
+  
+console.log(productName);
+'Droid'
+
+console.log(pricePerItem);
+2000
+
+
+// Задание 2
+
+// Start code
+let productName = 'Droid';
+let pricePerItem = 2000;
+
+// Write your code under this line
+productName = 'Repair droid';
+pricePerItem = 3500;
+ 
+
+// Задание 3
+
+const topSpeed = 160;
+const distance = 617.54;
+const login = 'mango935';
+const isOnline = true;
+const isAdmin = false;
+
+
+// Задание 4
+
+// Базовый код
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Пиши код ниже этой строки
+const totalPrice = pricePerItem * orderedQuantity;
+
+
+// Задание 5
+
+// Базовый код
+const productName = 'Дроид';
+const pricePerItem = 3500;
+
+// Пиши код ниже этой строки
+const message = `Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+
+
+// Задание 6
+
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const  totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+
+const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+
+// Задание 7
+
+// Пиши код ниже этой строки
+function sayHi() {
+  console.log('Привет, это моя первая функция!');
+}
+
+sayHi();
+
+
+// Задание 8
+
+// Пиши код ниже этой строки
+function add(a, b, c) {
+  console.log(`Результат сложения равен ${a + b + c}`);
+  // Пиши код выше этой строки
+}
+  
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
+
+
+// Задание 9
+
+function add(a, b, c) {
+  //  Пиши код ниже этой строки
+  return a + b + c;
+  console.log();
+  
+//  Пиши код выше этой строки
+}
+
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
+
+
+// Задание 10
+
+function makeMessage (name, price) {
+  // Пиши код ниже этой строки
+   const message = `Вы выбрали ${name}, цена за штуку ${price} кредитов`;
+  // Пиши код выше этой строки
+  return message;
+};
+
+
+// Задание 11
+
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  // Пиши код ниже этой строки
+  const totalPrice = orderedQuantity * pricePerItem;
+
+  // Пиши код выше этой строки
+  return totalPrice;
+};
+
+
+// Задание 12
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  // Пиши код ниже этой строки
+
+  const message = `Вы заказали дроидов на сумму ${orderedQuantity * pricePerDroid + deliveryFee} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+  // Пиши код выше этой строки
+  return message;
+}
+
+
+// Задание 13
+
+function isAdult(age) {
+  // Пиши код ниже этой строки
+  const passed = age >= 18;
+
+  // Пиши код выше этой строки
+  return passed;
+}
+
+
+// Задание 14
+// Задание 15
+
+// Задание 16
+// Задание 17
+// Задание 18
+// Задание 19
+// Задание 20
+// Задание 21
+// Задание 22
+// Задание 23
+
+// Задание 24
+// Задание 25
+// Задание 26
+// Задание 27
+// Задание 28
+// Задание 29
+// Задание 30
+// Задание 31
+// Задание 32
+// Задание 33
+// Задание 34
+// Задание 35
+// Задание 36
+
+
