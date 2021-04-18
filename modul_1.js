0bff44ccbf7ea5ab01f87af38c1ea1ecd366904e
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-04-18 22:10:27 +0300

    модуль-1, 36 задач

--- modul_1.js
+++ modul_1.js
@@ -146,29 +146,352 @@ function isAdult(age) {
 
 
 // Задание 14
+
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Пиши код ниже этой строки
+  const isMatch = password === 'jqueryismyjam';
+
+  // Пиши код выше этой строки
+  return isMatch;
+}
+
+
 // Задание 15
 
+function checkAge(age) {
+  let message;
+
+  if (age >= 18) { // Дополни эту строку
+    message = 'Вы совершеннолетний человек';
+  } else {
+    message = 'Вы не совершеннолетний человек';
+  }
+
+  return message;
+}
+
+
 // Задание 16
+
+function checkStorage(available, ordered) {
+  let message;
+  // Пиши код ниже этой строки
+  
+  message = ordered > available ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер';
+  
+  // Пиши код выше этой строки
+  return message;
+}
+
+
 // Задание 17
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+// Пиши код ниже этой строки
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
+
+
 // Задание 18
+
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Пиши код ниже этой строки
+ const totalPrice = (pricePerDroid * orderedQuantity);
+ if (totalPrice > customerCredits){
+   message = 'Недостаточно средств на счету!';
+ } else {
+   message = `Вы купили ${orderedQuantity} дроидов, на счету осталось ${customerCredits - totalPrice} кредитов`;
+ }
+  // Пиши код выше этой строки
+  return message;
+}
+
+
 // Задание 19
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) { // Дополни эту строку
+    message =  'Отменено пользователем!';
+  } else if (password === 'jqueryismyjam') { // Дополни эту строку
+    message = 'Добро пожаловать!';
+  } else {
+    message = 'Доступ запрещен, неверный пароль!';
+  }
+
+  return message;
+}
+
+
 // Задание 20
+
+function checkStorage(available, ordered) {
+  let message;
+  // Пиши код ниже этой строки
+  if(ordered <= null) {
+    message = 'В заказе еще нет товаров';
+  } else if (ordered >= available) {
+    message = 'Слишком большой заказ, на складе недостаточно товаров!'
+  } else {
+    message ='Заказ оформлен, с вами свяжется менеджер';
+  }
+
+  // Пиши код выше этой строки
+  return message;
+}
+
+
 // Задание 21
+
+function isNumberInRange(start, end, number) {
+  const isInRange = number >= start && number <=end; // дополни эту строку
+
+  return isInRange;
+}
+
+
 // Задание 22
+
+function checkIfCanAccessContent(subType) {
+  const canAccessContent = subType === 'vip' || subType === 'pro'; // дополни эту строку
+
+  return canAccessContent;
+}
+
+
 // Задание 23
 
+function isNumberNotInRange(start, end, number) {
+  const isInRange = number >= start && number <= end;
+  const isNotInRange = !isInRange; // Дополни эту строку
+
+  return isNotInRange;
+}
+
+
 // Задание 24
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Пиши код ниже этой строки
+  if (totalSpent >= 50000) {
+    discount = GOLD_DISCOUNT;
+  } else if (totalSpent >= 20000 && totalSpent <= 50000) {
+    discount = SILVER_DISCOUNT;
+  } else if (totalSpent >= 5000 && totalSpent <= 20000) {
+    discount = BRONZE_DISCOUNT;
+  } else if (totalSpent < 5000) {
+    discount = BASE_DISCOUNT;
+  }
+
+  // Пиши код выше этой строки
+  return discount;
+}
+
 // Задание 25
+
+function checkStorage(available, ordered) {
+  let message;
+  // Пиши код ниже этой строки
+  
+ message = ordered > available ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер';
+
+  // Пиши код выше этой строки
+  return message;
+}
+
 // Задание 26
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+  // Пиши код ниже этой строки
+message = password === ADMIN_PASSWORD ? 'Доступ разрешен' : 'Доступ запрещен, неверный пароль!';
+  // Пиши код выше этой строки
+  return message;
+}
+
+
 // Задание 27
+
+function getSubscriptionPrice(type) {
+  let price;
+  // Пиши код ниже этой строки
+
+ switch (type) { // Дополни эту строку
+    case 'starter': // Дополни эту строку
+      price = 0; // Дополни эту строку
+      break;
+
+    case 'professional': // Дополни эту строку
+      price = 20; // Дополни эту строку
+      break;
+
+    case 'organization': // Дополни эту строку
+      price = 50; // Дополни эту строку
+      break;
+  }
+
+  // Пиши код выше этой строки
+  return price;
+}
+
+
 // Задание 28
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  switch(password) {
+    case null:
+    message='Отменено пользователем!';
+    break;
+    case ADMIN_PASSWORD:
+    message ='Добро пожаловать!';
+    break;
+    default: message = 'Доступ запрещен, неверный пароль!';}
+  return message;
+}
+
+
 // Задание 29
+
+function getShippingCost(country) {
+  let message;
+  // Пиши код ниже этой строки
+ let price;
+ switch (country) {
+    case 'Китай':
+    price = 100;
+    break;
+     
+    case 'Чили':
+    price = 250;
+    break;
+     
+    case 'Австралия':
+    price = 170;
+    break;
+     
+    case 'Ямайка':
+    price = 120;
+    break;
+     
+    default: 
+     return 'Извините, в вашу страну доставки нет';
+ }
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+  
+  // Пиши код выше этой строки
+  return message;
+}
+
+
 // Задание 30
+
+function getNameLength(name) {
+  const message = `Длина вашего имени ${name.length} символа(ов)`; // Дополни эту строку
+
+  return message;
+}
+
+
 // Задание 31
+
+const courseTopic = 'JavaSript для начинающих';
+// Пиши код ниже этой строки
+
+const courseTopicLength = courseTopic.length;
+console.log('courseTopicLength', courseTopicLength);
+const firstElement = courseTopic[0];
+console.log('firstElement: ', firstElement);
+const lastElement = courseTopic[courseTopic.length-1];
+
+
+// Пиши код выше этой строки
+
+
 // Задание 32
+
+function getSubstring(string, length) {
+  const substring = string.slice(0, length); // Дополни эту строку
+  return substring;
+}
+console.log(getSubstring('Привет мир', 3));
+console.log(getSubstring('Привет мир', 6));
+console.log(getSubstring('Привет мир', 8));
+console.log(getSubstring('Привет мир', 10));
+console.log(getSubstring('Привет мир', 0));
+
+
 // Задание 33
+
+function formatMessage(message, maxLength) {
+  let result;
+// Пиши код ниже этой строки
+if(message.length <= maxLength) {
+  result = message;
+} else {
+  result = message.slice(0, maxLength) + '...';
+}
+// Пиши код выше этой строки
+  return result;
+}
+
+
 // Задание 34
+
+function normalizeInput(input) {
+  const normalizedInput = input.toLowerCase(); // Дополни эту строку
+  return normalizedInput;
+}
+
+
 // Задание 35
+
+function checkForName(fullName, name) {
+ const result = fullName.includes(name); // Дополни эту строку
+  return result;
+}
+
+
 // Задание 36
 
+function checkForSpam(message) {
+  let result;
+  // Пиши код ниже этой строки
+ const normalizedInput = message.toLowerCase();
+  if (normalizedInput.includes('sale')) {
+    result = true;
+  } else if (normalizedInput.includes('spam')) {
+    result = true;
+  } else {
+    result = false;
+  }
+    
+  // Пиши код выше этой строки
+  return result;
+}
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
 
